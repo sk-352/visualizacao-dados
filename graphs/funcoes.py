0a3fa083722e5f456f30d2f5f2b56aa31c09cd42
sk-352 <franciscosovinski@gmail.com>
2025-10-20 20:49:52 -0300

    Mudanças a serem submetidas: 	modified:   graphs/funcoes.py 	modified:   graphs/visualizacao_de_dados.py

--- graphs/funcoes.py
+++ graphs/funcoes.py
@@ -1,28 +1,28 @@
 import pandas as pd
 
-df = pd.read_csv("ecommerce_estatistica.csv")
+df = pd.read_csv('ecommerce_estatistica.csv')
 
 
 # Print informações importantes
 def print_infos():
-    pd.set_option("display.max_columns", None)
+    pd.set_option('display.max_columns', None)
     print(df.head())
-    print(f"\nInformações do DataFrame: {df.info()}")
-    print(f"\nDescrição do DataFrame: {df.describe()}")
-    print(f"\nValores únicos: {df.nunique()}")
+    print(f'\nInformações do DataFrame: {df.info()}')
+    print(f'\nDescrição do DataFrame: {df.describe()}')
+    print(f'\nValores únicos: {df.nunique()}')
 
 
 # Renomear os indices
 def renomear(df):
-    x_genero = df["Gênero"].value_counts().index
-    y_genero = df["Gênero"].value_counts().values
+    x_genero = df['Gênero'].value_counts().index
+    y_genero = df['Gênero'].value_counts().values
     dados_genero = pd.Series(y_genero, index=x_genero)
 
     # Renomeando os indices
     dados_genero = dados_genero.rename(
         index={
-            "Sem gênero infantil": "Infantil Genérico",
-            "roupa para gordinha pluss P ao 52": "Plus Size",
+            'Sem gênero infantil': 'Infantil Genérico',
+            'roupa para gordinha pluss P ao 52': 'Plus Size',
         }
     )
     return dados_genero
@@ -30,8 +30,8 @@ def renomear(df):
 
 # Nova categoria: Outros
 def outros(df):
-    x_temp = df["Temporada"].value_counts().index
-    y_temp = df["Temporada"].value_counts().values
+    x_temp = df['Temporada'].value_counts().index
+    y_temp = df['Temporada'].value_counts().values
     dados_temp = pd.Series(y_temp, index=x_temp)
 
     # Define o limite de 5% do total
@@ -43,17 +43,17 @@ def outros(df):
 
     # Adiciona a categoria 'Outros'
     if not menores.empty:
-        maiores["Outros"] = menores.sum()
+        maiores['Outros'] = menores.sum()
 
     return maiores
 
 
 # Removendo outliers com IQR
 def rm_outliers(df):
-    Avalicoes_Q1 = df["N_Avaliações"].quantile(0.25)
-    Avalicoes_Q3 = df["N_Avaliações"].quantile(0.75)
-    Vendidos_Q1 = df["Qtd_Vendidos_Cod"].quantile(0.25)
-    Vendidos_Q3 = df["Qtd_Vendidos_Cod"].quantile(0.75)
+    Avalicoes_Q1 = df['N_Avaliações'].quantile(0.25)
+    Avalicoes_Q3 = df['N_Avaliações'].quantile(0.75)
+    Vendidos_Q1 = df['Qtd_Vendidos_Cod'].quantile(0.25)
+    Vendidos_Q3 = df['Qtd_Vendidos_Cod'].quantile(0.75)
 
     IQR_Avalicoes = Avalicoes_Q3 - Avalicoes_Q1
     IQR_Vendidos = Vendidos_Q3 - Vendidos_Q1
@@ -63,8 +63,8 @@ def rm_outliers(df):
     limite_alto_Vendidos = Vendidos_Q3 + 1.5 * IQR_Vendidos
 
     df_rm_outliers = df[
-        (df["N_Avaliações"] <= limite_alto_Avalicoes)
-        & (df["Qtd_Vendidos_Cod"] <= limite_alto_Vendidos)
+        (df['N_Avaliações'] <= limite_alto_Avalicoes)
+        & (df['Qtd_Vendidos_Cod'] <= limite_alto_Vendidos)
     ]
 
     return df_rm_outliers
--- graphs/visualizacao_de_dados.py
+++ graphs/visualizacao_de_dados.py
@@ -1,72 +1,39 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.express as px
 from funcoes import print_infos, renomear, outros, rm_outliers
 
-df = pd.read_csv('ecommerce_estatistica.csv')
+df = pd.read_csv("ecommerce_estatistica.csv")
 
 print_infos()
 
 
 # Mudando estilo de gráficos
-sns.set_style('darkgrid')
-sns.set_palette('pastel')
+sns.set_style("darkgrid")
+sns.set_palette("pastel")
 
 # Mapa de calor - Correlação entre variáveis
 df_corr = df[
     [
-        'Nota_MinMax',
-        'N_Avaliações_MinMax',
-        'Desconto_MinMax',
-        'Preço_MinMax',
-        'Marca_Cod',
-        'Material_Cod',
-        'Temporada_Cod',
-        'Qtd_Vendidos_Cod',
-        'Marca_Freq',
-        'Material_Freq',
+        "Nota_MinMax",
+        "N_Avaliações_MinMax",
+        "Desconto_MinMax",
+        "Preço_MinMax",
+        "Marca_Cod",
+        "Material_Cod",
+        "Temporada_Cod",
+        "Qtd_Vendidos_Cod",
+        "Marca_Freq",
+        "Material_Freq",
     ]
 ].corr()
 
 plt.figure(figsize=(12, 12))
-sns.heatmap(df_corr, annot=True, fmt='.2f')
-plt.title('Mapa de Calor - Correlação entre Variáveis')
+sns.heatmap(df_corr, annot=True, fmt=".2f")
+plt.title("Mapa de Calor - Correlação entre Variáveis")
 plt.xticks(rotation=-28)
 plt.tight_layout()
-plt.show()
-
-
-# Gráfico de Histograma - Distribuição de Preços
-plt.figure(figsize=(10, 6))
-plt.hist(df['Preço'], bins=50, color='green', alpha=0.8)
-plt.title('Histograma - Distribuição de Preços')
-plt.xlabel('Preços dos Produtos em R$')
-plt.xticks(ticks=range(0, int(df['Preço'].max()) + 50, 50))
-plt.ylabel('Frequência')
-plt.grid(True)
-plt.show()
-
-
-# Gráfico de dispersão - Nota MinMax x N_Avaliações_MinMax
-sns.jointplot(
-    x='Nota',
-    y='N_Avaliações',
-    data=df,
-    kind='scatter',
-    color='#863e9c',
-)
-plt.xlabel('Notas')
-plt.ylabel('Número de Avaliações')
-plt.show()
-
-
-# Gráfico de barra - Gênero
-dados_genero = renomear(df)
-
-plt.figure(figsize=(12, 6))
-plt.bar(dados_genero.index, dados_genero.values, color='#99b595')
-plt.title('Barras - Distribuição da Categoria "Gênero"')
-plt.ylabel('Quantidade')
 plt.show()
 
 
@@ -74,31 +41,75 @@ plt.show()
 maiores = outros(df)
 
 plt.figure(figsize=(8, 6))
-plt.pie(maiores, labels=maiores.index, autopct='%1.1f%%', startangle=90)
-plt.title('Pizza - Distribuição da Categoria Temporada')
+plt.pie(maiores, labels=maiores.index, autopct="%1.1f%%", startangle=90)
+plt.title("Pizza - Distribuição da Categoria Temporada")
 plt.show()
 
 
 # Gráfico de densidade
 plt.figure(figsize=(10, 6))
-sns.kdeplot(df['Nota'], fill=True, color='#863e9c')
-plt.title('Densidade - Distribuição de Notas')
-plt.xlabel('Nota')
-plt.ylabel('Densidade')
+sns.kdeplot(df["Nota"], fill=True, color="#863e9c")
+plt.title("Densidade - Distribuição de Notas")
+plt.xlabel("Nota")
+plt.ylabel("Densidade")
 plt.show()
 
 
-# Gráfico de Regressão - N_Avaliações_MinMax x Qtd_Vendidos_Cod
+# Gráfico de Histograma - Distribuição de Preços
+fig = px.histogram(
+    data_frame=df,
+    x="Preço",
+    title="Histograma - Distribuição de Preços",
+    nbins=50,
+    color_discrete_sequence=["#99b595"],
+    facet_col_spacing=0.1,
+)
+fig.show()
+
+
+# Gráfico de dispersão - Notas x N Avaliações
+fig = px.scatter(
+    data_frame=df,
+    x="Nota",
+    y="N_Avaliações",
+    labels={"Nota": "Notas", "N_Avaliações": "Número de Avaliações"},
+    title="Gráfico de Dispersão - Notas x N° Avaliações",
+    size="N_Avaliações",
+    size_max=50,
+    opacity=0.8,
+)
+fig.show()
+
+
+# Gráfico de barra - Gênero
+dados_genero = renomear(df)
+
+fig = px.bar(
+    data_frame=dados_genero,
+    x=dados_genero.index,
+    y=dados_genero.values,
+    labels={"x": "Gênero", "y": "Quantidade"},
+    title='Barras - Distribuição da Categoria "Gênero"',
+    color=dados_genero.index,
+    color_discrete_sequence=px.colors.qualitative.Set3,
+)
+fig.show()
+
+
+# Grafico de Regressão - N_Avaliações x Qtd_Vendidos_Cod
 df_rm_outliers = rm_outliers(df)
 
-plt.figure(figsize=(10, 6))
-sns.regplot(
-    x='N_Avaliações',
-    y='Qtd_Vendidos_Cod',
-    data=df_rm_outliers,
-    color='#278f65',
+fig = px.scatter(
+    data_frame=df_rm_outliers,
+    x="N_Avaliações",
+    y="Qtd_Vendidos_Cod",
+    size="N_Avaliações",
+    labels={
+        "N_Avaliações": "Número de Avaliações",
+        "Qtd_Vendidos_Cod": "Quantidade Vendida",
+    },
+    title="Regressão - Número de Avaliações x Quantidade Vendida",
+    trendline="ols",
+    trendline_color_override="#eb8c00"
 )
-plt.title('Regressão - Número de Avaliações x Quantidade Vendida')
-plt.xlabel('Número de Avaliações')
-plt.ylabel('Quantidade Vendida')
-plt.show()
+fig.show()
