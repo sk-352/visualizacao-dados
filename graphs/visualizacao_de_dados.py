9af3fbdacac4ca18e6c66bab27c418253f87ee30
sk-352 <franciscosovinski@gmail.com>
2025-12-09 14:58:28 -0300

    Atualizando os gráficos inteiramente para o plotly.express Mudanças a serem submetidas: 	modified:   graphs/visualizacao_de_dados.py

--- graphs/visualizacao_de_dados.py
+++ graphs/visualizacao_de_dados.py
@@ -1,6 +1,5 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+from dash import Dash, dcc, html
 import plotly.express as px
 from funcoes import print_infos, renomear, outros, rm_outliers
 
@@ -9,107 +8,172 @@ df = pd.read_csv("ecommerce_estatistica.csv")
 print_infos()
 
 
-# Mudando estilo de gráficos
-sns.set_style("darkgrid")
-sns.set_palette("pastel")
-
 # Mapa de calor - Correlação entre variáveis
-df_corr = df[
-    [
-        "Nota_MinMax",
-        "N_Avaliações_MinMax",
-        "Desconto_MinMax",
-        "Preço_MinMax",
-        "Marca_Cod",
-        "Material_Cod",
-        "Temporada_Cod",
-        "Qtd_Vendidos_Cod",
-        "Marca_Freq",
-        "Material_Freq",
-    ]
-].corr()
+def plot_heatmap(df):
+    df_corr = df[
+        [
+            "Nota_MinMax",
+            "N_Avaliações_MinMax",
+            "Desconto_MinMax",
+            "Preço_MinMax",
+            "Marca_Cod",
+            "Material_Cod",
+            "Temporada_Cod",
+            "Qtd_Vendidos_Cod",
+            "Marca_Freq",
+            "Material_Freq",
+        ]
+    ].corr()
 
-plt.figure(figsize=(12, 12))
-sns.heatmap(df_corr, annot=True, fmt=".2f")
-plt.title("Mapa de Calor - Correlação entre Variáveis")
-plt.xticks(rotation=-28)
-plt.tight_layout()
-plt.show()
+    fig_heatmap = px.imshow(
+        df_corr,
+        title="Mapa de Calor - Correlação entre Variáveis",
+        color_continuous_scale="Viridis",
+        range_color=[-1, 1],
+        labels=dict(
+            x="Colunas",
+            y="Linhas",
+            color="Correlação",
+        ),
+    )
+    return fig_heatmap
 
 
 # Gráfico de pizza - Distribuição da Categoria Temporada
+
 maiores = outros(df)
 
-plt.figure(figsize=(8, 6))
-plt.pie(maiores, labels=maiores.index, autopct="%1.1f%%", startangle=90)
-plt.title("Pizza - Distribuição da Categoria Temporada")
-plt.show()
+
+def plot_pizza(maiores):
+    fig_pizza = px.pie(
+        values=maiores.values,
+        names=maiores.index,
+        title="Pizza - Distribuição da Categoria Temporada",
+        hole=0.25,
+    )
+    return fig_pizza
 
 
 # Gráfico de densidade - Distribuição de Notas
-plt.figure(figsize=(10, 6))
-sns.kdeplot(df["Nota"], fill=True, color="#863e9c")
-plt.title("Densidade - Distribuição de Notas")
-plt.xlabel("Nota")
-plt.ylabel("Densidade")
-plt.show()
+def plot_density(df):
+
+    fig_density = px.histogram(
+        data_frame=df,
+        x="Nota",
+        marginal="violin",
+        color_discrete_sequence=["#99b595"],
+        title="Densidade - Distribuição de Notas",
+    )
+    fig_density.update_layout(
+        xaxis_title="Notas",
+        yaxis_title="Quantidade de Avaliações",
+    )
+
+    return fig_density
 
 
 # Gráfico de Histograma - Distribuição de Preços
-fig = px.histogram(
-    data_frame=df,
-    x="Preço",
-    title="Histograma - Distribuição de Preços",
-    nbins=50,
-    color_discrete_sequence=["#99b595"],
-    facet_col_spacing=0.1,
-)
-fig.show()
+def plot_histogram(df):
+    fig_hist = px.histogram(
+        data_frame=df,
+        x="Preço",
+        title="Histograma - Distribuição de Preços",
+        nbins=50,
+        color_discrete_sequence=["#99b595"],
+        facet_col_spacing=0.1,
+    )
+    fig_hist.update_layout(
+        xaxis_title="Preços",
+        yaxis_title="Quantidade de Produtos na Faixa de Preço",
+    )
+
+    return fig_hist
 
 
 # Gráfico de dispersão - Notas x N Avaliações
-fig = px.scatter(
-    data_frame=df,
-    x="Nota",
-    y="N_Avaliações",
-    labels={"Nota": "Notas", "N_Avaliações": "Número de Avaliações"},
-    title="Gráfico de Dispersão - Notas x N° Avaliações",
-    size="N_Avaliações",
-    size_max=50,
-    opacity=0.8,
-)
-fig.show()
+def plot_scatter(df):
+    fig_scatter = px.scatter(
+        data_frame=df,
+        x="Nota",
+        y="N_Avaliações",
+        labels={"Nota": "Notas", "N_Avaliações": "Número de Avaliações"},
+        title="Gráfico de Dispersão - Notas x N° Avaliações",
+        size="N_Avaliações",
+        size_max=50,
+        opacity=0.8,
+    )
+    return fig_scatter
 
 
 # Gráfico de barra - Gênero
 dados_genero = renomear(df)
 
-fig = px.bar(
-    data_frame=dados_genero,
-    x=dados_genero.index,
-    y=dados_genero.values,
-    labels={"x": "Gênero", "y": "Quantidade"},
-    title='Barras - Distribuição da Categoria "Gênero"',
-    color=dados_genero.index,
-    color_discrete_sequence=px.colors.qualitative.Set3,
-)
-fig.show()
+
+def plot_bar(dados_genero):
+    fig_bar = px.bar(
+        data_frame=dados_genero,
+        x=dados_genero.index,
+        y=dados_genero.values,
+        labels={"x": "Gênero", "y": "Quantidade"},
+        title='Barras - Distribuição da Categoria "Gênero"',
+        color=dados_genero.index,
+        color_discrete_sequence=px.colors.qualitative.Set3,
+    )
+    return fig_bar
 
 
 # Grafico de Regressão - N_Avaliações x Qtd_Vendidos_Cod
 df_rm_outliers = rm_outliers(df)
 
-fig = px.scatter(
-    data_frame=df_rm_outliers,
-    x="N_Avaliações",
-    y="Qtd_Vendidos_Cod",
-    size="N_Avaliações",
-    labels={
-        "N_Avaliações": "Número de Avaliações",
-        "Qtd_Vendidos_Cod": "Quantidade Vendida",
-    },
-    title="Regressão - Número de Avaliações x Quantidade Vendida",
-    trendline="ols",
-    trendline_color_override="#eb8c00"
-)
-fig.show()
+
+def plot_regression(df_rm_outliers):
+    fig_regression = px.scatter(
+        data_frame=df_rm_outliers,
+        x="N_Avaliações",
+        y="Qtd_Vendidos_Cod",
+        size="N_Avaliações",
+        labels={
+            "N_Avaliações": "Número de Avaliações",
+            "Qtd_Vendidos_Cod": "Quantidade Vendida",
+        },
+        title="Regressão - Número de Avaliações x Quantidade Vendida",
+        trendline="ols",
+        trendline_color_override="#eb8c00",
+    )
+    return fig_regression
+
+
+def cria_app(df):
+    # Cria app
+    app = Dash(__name__)
+
+    fig_heatmap = plot_heatmap(df)
+    fig_pizza = plot_pizza(maiores)
+    fig_density = plot_density(df)
+    fig_hist = plot_histogram(df)
+    fig_scatter = plot_scatter(df)
+    fig_bar = plot_bar(dados_genero)
+    fig_regression = plot_regression(df_rm_outliers)
+
+    # Layout do app
+    app.layout = html.Div(children=[
+        html.Div(style={'flex': '1'}, children=[
+            dcc.Graph(figure=fig_heatmap),
+            dcc.Graph(figure=fig_pizza),
+        ]),
+        html.Div(style={'flex': '1'}, children=[
+            dcc.Graph(figure=fig_density),
+            dcc.Graph(figure=fig_hist),
+        ]),
+        html.Div(style={'flex': '1'}, children=[
+            dcc.Graph(figure=fig_scatter),
+            dcc.Graph(figure=fig_bar),
+        ]),
+        dcc.Graph(figure=fig_regression),
+    ])
+
+    return app
+
+if __name__ == '__main__':
+    app = cria_app(df)
+    app.run(debug=True, port=8050)
